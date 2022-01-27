5d689a581f444e21d8e6ded93dcff118d9d826d9
Jack Kitto <jack.kitt0@gmail.com>
2022-01-27 14:10:55 +1100

    prisma1

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const Web3 = require('web3');
-const conn = require('./web3');
+const { prisma } = require('./generated/prisma-client')
 
+// A `main` function so that we can use async/await
+async function main() {
+  // Create a new user called `Alice`
+  const newUser = await prisma.createUser({ name: 'Alice' })
+  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+
+  // Read all users from the database and print them to the console
+  const allUsers = await prisma.users()
+  console.log(allUsers)
+}
+
+main().catch(e => console.error(e))
